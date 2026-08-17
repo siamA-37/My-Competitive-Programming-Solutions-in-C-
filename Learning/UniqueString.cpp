4a877d0af86b1b786fa2af8cb8ac6bed59af2e1d
siamA-37 <siamssnic2006@gmail.com>
2026-08-17 05:48:51 +0000

    Add string related important stl in c++

---
+++ Learning/UniqueString.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n';
+int main()
+{
+    string s="bbddaacc";
+    sort(s.begin(),s.end());
+    cout<<"Sorted String : ";
+    cout<<s<<endl;
+    int sz = unique(s.begin(),s.end())-s.begin();
+    cout<<"Unique String is : ";
+    for(int i=0;i<sz;i++)cout<<s[i];
+    cout<<endl;
+    cout<<"------------------Max element and Min Element------------\n\n";
+    auto mx=max_element(s.begin(),s.end());
+    auto mn =min_element(s.begin(),s.end());
+    cout<<"Max Element : "<<*mx<<" Min Element : "<<*mn<<endl;
+    cout<<"------------Erase a character------------"<<endl;
+    s.erase(s.begin());
+    cout<<s<<endl;
+    //Remove all same character 
+    s="aabbbssskk";
+    s.erase(remove(s.begin(),s.end(),'s'),s.begin()+6);
+    cout<<s<<endl;
+    return 0;
+}
